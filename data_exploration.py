615a6800992b17dded2342bdb5a7774c66122787
Kpew <131501695+Kpewww@users.noreply.github.com>
2024-05-29 18:31:05 -0700

    Update data_exploration.py

--- data_exploration.py
+++ data_exploration.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-data = pd.read_csv('/Users/will./Desktop/food_safety_ml/mushrooms.csv')
+data = pd.read_csv('./mushrooms.csv')
 data.columns = [col.strip() for col in data.columns]
 null_data_count = data.isnull().sum().sum()
 if (null_data_count == 0):
